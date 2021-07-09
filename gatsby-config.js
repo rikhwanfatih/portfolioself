283f8aa2fb96280eb31ac7beb09be2fff775cc71
rikhwanfatih <87036188+rikhwanfatih@users.noreply.github.com>
2021-07-10 01:42:34 +0700

    Changes

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    siteMetadata: {
+        title: "Quarkly gatsby project",
+        description: "",
+        author: "@Rikhwan Fiardhi",
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-resolve-src",
+        "gatsby-plugin-meta-redirect",
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // "gatsby-plugin-offline",
+    ],
+};
---
+++ qapi.js
@@ -0,0 +1,41 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60e48e3b76068b001759f328",
+				"60e48e3b76068b001759f32a",
+				"60e4930ab42a9a0018930a74",
+				"60e49370f1262f001776921c"
+			]
+		},
+		"60e48e3b76068b001759f328": {
+			"id": "60e48e3b76068b001759f328",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60e48e3b76068b001759f32a": {
+			"id": "60e48e3b76068b001759f32a",
+			"name": "Home",
+			"pageUrl": "index"
+		},
+		"60e4930ab42a9a0018930a74": {
+			"id": "60e4930ab42a9a0018930a74",
+			"pageUrl": "about",
+			"name": "About"
+		},
+		"60e49370f1262f001776921c": {
+			"id": "60e49370f1262f001776921c",
+			"pageUrl": "gallery",
+			"name": "Gallery"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
