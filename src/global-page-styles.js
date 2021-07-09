9af078489088bb24e395e8130598ee9677391b4c
rikhwanfatih <87036188+rikhwanfatih@users.noreply.github.com>
2021-07-10 01:37:50 +0700

    Changes

---
+++ src/global-page-styles.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { transformVar } from "@quarkly/atomize";
+import { createGlobalStyle } from "styled-components";
+
+const pageStyles = {
+    "404": {},
+    "index": {},
+    "about": {},
+    "gallery": {}
+};
+
+const PageStyles = createGlobalStyle`
+    body {
+        ${({ styles }) =>
+            Object.entries(styles || {}).map(
+                ([prop, value]) => `${prop}: ${transformVar(prop, value)};`
+            )}
+    }
+`;
+export const GlobalQuarklyPageStyles = ({ pageUrl }) => <PageStyles styles={pageStyles[pageUrl]} />
